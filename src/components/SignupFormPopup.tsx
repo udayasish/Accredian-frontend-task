8ef2ac7e79c46ee94d57913f487e92bbf10422a4
Udayasish Bora <32497837+udayasish@users.noreply.github.com>
2024-07-12 14:13:40 +0530

    Update SignupFormPopup.tsx

--- src/components/SignupFormPopup.tsx
+++ src/components/SignupFormPopup.tsx
@@ -44,7 +44,7 @@ const SignupFormPopup: React.FC<SignupFormPopupProps> = ({ isVisible, onClose })
     e.preventDefault();
     try {
       // const response = await fetch('https://accredian-backend-task-c7o2.onrender.com/api/referrals', {
-      const response = await fetch('http://localhost:8000/api/referrals', {
+      const response = await fetch('https://accredian-backend-task-c7o2.onrender.com/api/referrals', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
